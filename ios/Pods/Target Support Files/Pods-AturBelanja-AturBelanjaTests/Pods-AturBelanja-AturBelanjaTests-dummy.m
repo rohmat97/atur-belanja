70ed14ff684a3b9348e49775f34820a348e89e75
rohmat97 <rohmat661@gmail.com>
2020-09-15 06:05:03 +0700

    update splashscreen ios

---
+++ ios/Pods/Target Support Files/Pods-AturBelanja-AturBelanjaTests/Pods-AturBelanja-AturBelanjaTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AturBelanja_AturBelanjaTests : NSObject
+@end
+@implementation PodsDummy_Pods_AturBelanja_AturBelanjaTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-AturBelanja/Pods-AturBelanja-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AturBelanja : NSObject
+@end
+@implementation PodsDummy_Pods_AturBelanja
+@end
